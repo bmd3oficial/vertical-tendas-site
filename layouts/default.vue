156a53168245201bf4f8ce70773ce7ea77c9cc6f
caua <cauaalvarenganeves@gmail.com>
2025-06-24 14:19:15 -0300

    fix: menu com margem

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,7 +27,7 @@ function ensureHttps(url: string): string {
     <header
       class="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
     >
-      <nav class="flex justify-between w-full items-center px-0 py-2 sm:px-4 md:px-6">
+      <nav class="flex justify-between w-full items-center px-4 py-2 sm:px-6 md:px-8 lg:px-12">
         <NuxtLink
           :to="{ name: 'index' }"
           class="mr-6 flex items-center space-x-2"
